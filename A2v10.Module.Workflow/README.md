fdd870590d18ab6bb17290e014e86dac3c3aa726
alex-kukhtin <alex.kukhtin@outlook.com>
2025-05-12 14:23:37 +0300

    Add project files.

---
+++ A2v10.Module.Workflow/README.md
@@ -0,0 +1,3 @@
+﻿# A2v10.Module.Workflow
+
+Workflow UI module for A2v10 application
---
+++ A2v10.Standard.Modules/WebApp/Startup.cs
@@ -0,0 +1,32 @@
+﻿// Copyright © 2024 Oleksandr Kukhtin. All rights reserved.
+
+using A2v10.ReportEngine.Pdf;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp
+{
+    public class Startup(IConfiguration configuration)
+    {
+        public IConfiguration Configuration { get; } = configuration;
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.UseAppRuntimeBuilder(); // Before platform!
+
+            services.UsePlatform(Configuration);
+
+            services.AddReportEngines(factory =>
+            {
+                factory.RegisterEngine<PdfReportEngine>("pdf");
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.ConfigurePlatform(env, Configuration);
+        }
+    }
+}
