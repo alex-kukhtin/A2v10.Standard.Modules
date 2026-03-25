4c1c05ca32ecae0725f7583c04b4f70c1302c63d
alex-kukhtin <alex.kukhtin@outlook.com>
2026-03-25 21:24:23 +0200

    v 1095

---
+++ MainApp/html/dynamic/index.template.js
@@ -0,0 +1,26 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    const template = {
+        options: {
+            noDirty: true
+        },
+        properties: {
+            'TRoot.$Text': String
+        },
+        commands: {
+            setHtml,
+        },
+        delegates: {
+            testClick
+        }
+    };
+    exports.default = template;
+    function setHtml() {
+        this.$Text = `Текст з <a href="" data-info='{"action": "report", "id": 77}'>прикладом посилання</a>`;
+    }
+    function testClick(elem, name) {
+        console.log('delegate', this.$ctrl, elem, name);
+        console.log(JSON.parse(elem.dataset.info));
+    }
+});
---
+++ MainApp/html/dynamic/index.template.ts
@@ -0,0 +1,27 @@
+﻿//testpdf.template
+
+const template: Template = {
+	options: {
+		noDirty: true
+	},
+	properties: {
+		'TRoot.$Text': String
+	},
+	commands: {
+		setHtml,
+	},
+	delegates: {
+		testClick
+	}
+};
+
+export default template;
+
+function setHtml() {
+	this.$Text = `Текст з <a href="" data-info='{"action": "report", "id": 77}'>прикладом посилання</a>`
+}
+
+function testClick(elem: HTMLElement, name) {
+	console.log('delegate', this.$ctrl, elem, name);
+	console.log(JSON.parse(elem.dataset.info));
+}
